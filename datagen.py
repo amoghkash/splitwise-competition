499b463092a99e6e696bf1009711869f43ecd335
Amogh Kashyap <94269573+amoghkash@users.noreply.github.com>
2024-12-23 17:13:37 -0400

    Converted to controllable seeded randomizer

--- datagen.py
+++ datagen.py
@@ -1,12 +1,13 @@
 import csv
-from random import randint, random
+import random
 import math
 
+random.seed(10)
 max_transaction_value = 200
 
 if (input("Randomize Values? y or n\n") == "y"):
-    people_count = randint(3,25)
-    transaction_count = randint(25,250)
+    people_count = random.randint(3,25)
+    transaction_count = random.randint(25,250)
 else:
     people_count = int(input("How many people involved?\n"))
     transaction_count = int(input("How many total transactions?\n"))
@@ -17,7 +18,7 @@ with open('./sample_data.csv', 'w', newline='') as file:
     writer.writerow(field)
 
     for i in range(transaction_count):
-        person_name = "Person" + str(randint(1, people_count))
-        transaction_value = round((random() * max_transaction_value) + 1,2)
+        person_name = "Person" + str(random.randint(1, people_count))
+        transaction_value = round((random.random() * max_transaction_value) + 1,2)
         datapoint = [person_name, transaction_value]
         writer.writerow(datapoint)
